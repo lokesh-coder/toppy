6dd714ae8a6235162f038943da3e9ac4f416da9c
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-11-02 04:57:42 +0530

    fix: archive version before publishing

---
+++ bin/archive.js
@@ -0,0 +1,19 @@
+const fs = require('fs-extra');
+const version = process.argv[2];
+const archiveMetaFile = './docs/assets/archived-versions.json';
+
+async function archiveFiles() {
+  try {
+    await fs.copy('./dist/toppy-app', `./dist/${version}`);
+    await fs.copy(`./dist/${version}`, `./dist/toppy-app/${version}`);
+    let archivedData = await fs.readJson(archiveMetaFile);
+    archivedData = archivedData || {};
+    archivedData[version] = Date.now();
+    await fs.writeJson(archiveMetaFile, archivedData);
+    console.log('successfully archived!');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+archiveFiles();
--- bin/gh-pages.js
+++ bin/gh-pages.js
@@ -1,33 +1,14 @@
 const ghpages = require('gh-pages');
-const fs = require('fs-extra');
-const version = process.argv[2];
-const archiveMetaFile = './docs/assets/archived-versions.json';
-
-async function copyFiles() {
-  try {
-    await fs.copy('./dist/toppy-app', `./dist/${version}`);
-    await fs.copy(`./dist/${version}`, `./dist/toppy-app/${version}`);
-    let archivedData = await fs.readJson(archiveMetaFile);
-    archivedData = archivedData || {};
-    archivedData[version] = Date.now();
-    await fs.writeJson(archiveMetaFile, archivedData);
-    console.log('successfully archived!');
-    ghpages.publish(
-      './dist/toppy-app',
-      {
-        repo: 'https://' + process.env.GH_TOKEN + '@github.com/lokesh-coder/toppy.git'
-      },
-      function(err) {
-        if (err) {
-          console.log('Error occured during gh pages push', err);
-        } else {
-          console.log('Pushed to ghpages!');
-        }
-      }
-    );
-  } catch (err) {
-    console.error(err);
+ghpages.publish(
+  './dist/toppy-app',
+  {
+    repo: 'https://' + process.env.GH_TOKEN + '@github.com/lokesh-coder/toppy.git'
+  },
+  function(err) {
+    if (err) {
+      console.log('Error occured during gh pages push', err);
+    } else {
+      console.log('Pushed to ghpages!');
+    }
   }
-}
-
-copyFiles();
+);
