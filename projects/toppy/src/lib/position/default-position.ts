516ba9b261613557ec420abdae525cb97e983bfb
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-11-18 19:00:10 +0530

    feat: fade in content once position is updated

--- projects/toppy/src/lib/position/default-position.ts
+++ projects/toppy/src/lib/position/default-position.ts
@@ -5,6 +5,9 @@ export class DefaultPosition extends Position {
   constructor() {
     super();
   }
+  updateConfig(newConfig) {
+    return null;
+  }
   getPositions(hostElement?: HTMLElement): PositionCoOrds {
     return {};
   }
--- projects/toppy/src/lib/position/position.ts
+++ projects/toppy/src/lib/position/position.ts
@@ -2,6 +2,7 @@
 
 export abstract class Position {
   abstract getPositions(host: HTMLElement): any;
+  abstract updateConfig(config: object): any;
   getClassName(): string {
     return this.constructor.name.replace('Position', '-position').toLocaleLowerCase();
   }
