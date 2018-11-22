e8530d89fc49cf639957da5dc854ac2f496ecfaa
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-11-22 21:12:36 +0530

    docs: replace title input to heading

--- docs/app/utils/section/section.component.ts
+++ docs/app/utils/section/section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section',
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SectionComponent implements OnInit {
   @Input()
-  title;
+  heading;
   constructor() {}
 
   ngOnInit() {}
