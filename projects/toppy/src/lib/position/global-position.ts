54e7ed5fe83f2f6622d5f2686b1f4d5d43546fb2
lokesh_coder <mexican.dirtyfellow@gmail.com>
2018-12-05 10:22:45 +0530

    refactor: changed logic of calculation for global position

--- projects/toppy/src/lib/position/global-position.ts
+++ projects/toppy/src/lib/position/global-position.ts
@@ -9,63 +9,110 @@ export interface Config {
 }
 export class GlobalPosition extends Position {
   private size;
-  protected _config: Config = { placement: InsidePlacement.CENTER, hostWidth: 500, hostHeight: 500, offset: 0 };
+  protected _config: Config = { placement: InsidePlacement.CENTER, hostWidth: 100, hostHeight: 100, offset: 0 };
   constructor(config: Config) {
     super();
     this._config = { ...this._config, ...config };
   }
   getPositions(hostElement?: HTMLElement) {
-    const host = hostElement.getBoundingClientRect();
+    const host = hostElement.getBoundingClientRect() as any;
     const src = {
       width: (window as any).innerWidth,
       height: (window as any).innerHeight
     };
+    if (typeof this._config.hostHeight === 'number') {
+      host.height = this._config.hostHeight;
+    }
+    if (typeof this._config.hostWidth === 'number') {
+      host.width = this._config.hostWidth;
+    }
+    if (typeof this._config.hostWidth === 'string' && this._config.hostWidth.endsWith('%')) {
+      this._config.hostWidth = this._getPercentageToCssPx(src.width, this._config.hostWidth);
+    }
+    if (typeof this._config.hostHeight === 'string' && this._config.hostHeight.endsWith('%')) {
+      this._config.hostHeight = this._getPercentageToCssPx(src.height, this._config.hostHeight);
+    }
+
     const props = this[`calculate_${this._config.placement}`](src, host);
     return { ...props, width: this._config.hostWidth, height: this._config.hostHeight, position: 'fixed' };
   }
+  private _getPercentageToCssPx(max, percentage: string) {
+    let number = Number(percentage.slice(0, -1));
+    if (number > 100) {
+      number = 100;
+    }
+    return `calc(${max}px - ${100 - number}%)`;
+  }
+
+  private _calc(placement: InsidePlacement, src, host) {
+    const [main, sub] = placement.split('');
+    const p: any = {};
+
+    if (main === 't') {
+      p.top = this._config.offset;
+    }
+    if (main === 'b') {
+      p.bottom = this._config.offset;
+    }
+    if ((main === 'l' || main === 'r' || main === 'c') && !sub) {
+      p.top = (src.height - host.height) / 2;
+    }
+
+    if ((main === 't' || main === 'b' || main === 'c') && !sub) {
+      p.left = (src.width - host.width) / 2;
+    }
+    if ((main === 'l' && !sub) || sub === 'l') {
+      p.left = this._config.offset;
+    }
+    if ((main === 'r' && !sub) || sub === 'r') {
+      p.right = this._config.offset;
+    }
+
+    return p;
+  }
 
   private [`calculate_${InsidePlacement.TOP}`](src, host) {
-    const top = this._config.offset;
-    const left = (src.width - host.width) / 2;
+    const top = this._config.offset; //
+    const left = (src.width - host.width) / 2; //
     return { left, top };
   }
   private [`calculate_${InsidePlacement.BOTTOM}`](src, host) {
-    const bottom = this._config.offset;
-    const left = (src.width - host.width) / 2;
+    const bottom = this._config.offset; //
+    const left = (src.width - host.width) / 2; //
     return { left, bottom };
   }
   private [`calculate_${InsidePlacement.LEFT}`](src, host) {
-    const top = (src.height - host.height) / 2;
-    const left = this._config.offset;
+    const top = (src.height - host.height) / 2; //
+    const left = this._config.offset; //
     return { left, top };
   }
   private [`calculate_${InsidePlacement.RIGHT}`](src, host) {
-    const top = (src.height - host.height) / 2;
+    const top = (src.height - host.height) / 2; //
     const right = this._config.offset;
     return { right, top };
   }
   private [`calculate_${InsidePlacement.CENTER}`](src, host) {
-    const top = (src.height - host.height) / 2;
-    const left = (src.width - host.width) / 2;
+    const top = (src.height - host.height) / 2; //
+    const left = (src.width - host.width) / 2; //
     return { left, top };
   }
   private [`calculate_${InsidePlacement.TOP_LEFT}`](src, host) {
-    const top = this._config.offset;
-    const left = this._config.offset;
+    const top = this._config.offset; //
+    const left = this._config.offset; //
     return { left, top };
   }
   private [`calculate_${InsidePlacement.TOP_RIGHT}`](src, host) {
-    const top = this._config.offset;
+    const top = this._config.offset; //
     const right = this._config.offset;
     return { right, top };
   }
   private [`calculate_${InsidePlacement.BOTTOM_LEFT}`](src, host) {
-    const bottom = this._config.offset;
-    const left = this._config.offset;
+    const bottom = this._config.offset; //
+    const left = this._config.offset; //
     return { left, bottom };
   }
   private [`calculate_${InsidePlacement.BOTTOM_RIGHT}`](src, host) {
-    const bottom = this._config.offset;
+    const bottom = this._config.offset; //
     const right = this._config.offset;
     return { right, bottom };
   }
---
+++ projects/toppy/src/tests/positions/global-position.spec.ts
@@ -0,0 +1,193 @@
+/// <reference types="karma-viewport" />
+
+import { InsidePlacement } from '../../lib/models';
+import { GlobalPosition } from '../../lib/position';
+
+describe('== Global position ==', () => {
+  let targetElement: HTMLElement;
+  let hostElement: HTMLElement;
+  let ww;
+  let wh;
+  beforeEach(() => {
+    targetElement = document.createElement('div');
+    targetElement.setAttribute('class', 'foobar');
+    const textnode = document.createTextNode('Hello');
+    targetElement.appendChild(textnode);
+    document.getElementsByTagName('body')[0].appendChild(targetElement);
+
+    hostElement = document.createElement('div');
+    hostElement.setAttribute('class', 'hostelement');
+    const textnode2 = document.createTextNode('Im host');
+    hostElement.appendChild(textnode2);
+    document.getElementsByTagName('body')[0].appendChild(hostElement);
+    viewport.set(1000, 480);
+
+    ww = window.innerWidth;
+    wh = window.innerHeight;
+  });
+  afterEach(() => {
+    document.getElementsByTagName('body')[0].removeChild(targetElement);
+    document.getElementsByTagName('body')[0].removeChild(hostElement);
+    viewport.set(1000, 480);
+  });
+
+  it('should have target element in document', () => {
+    expect(document.querySelector('.foobar').textContent).toBe('Hello');
+  });
+  it('should get updated config', () => {
+    const gloPos = new GlobalPosition({});
+    gloPos.updateConfig({ offset: 2 });
+    expect((gloPos as any)._config).toEqual({
+      placement: InsidePlacement.CENTER,
+      hostWidth: 100,
+      hostHeight: 100,
+      offset: 2
+    });
+  });
+  it('should return correct class name', () => {
+    const gloPos = new GlobalPosition({});
+    expect(gloPos.getClassName()).toBe('global-position');
+  });
+
+  describe('should return correct position coords of host element', () => {
+    let srcCoords;
+    beforeEach(() => {
+      srcCoords = targetElement.getBoundingClientRect();
+    });
+    it('when exact width and height is provided', () => {
+      const gloPos = new GlobalPosition({
+        hostWidth: 4,
+        hostHeight: 10,
+        placement: InsidePlacement.TOP
+      });
+      expect(gloPos.getPositions(hostElement)).toEqual({
+        left: (ww - 4) / 2,
+        top: 0,
+        width: 4,
+        height: 10,
+        position: 'fixed'
+      });
+    });
+    it('when no width and height is provided', () => {
+      const gloPos = new GlobalPosition({ placement: InsidePlacement.TOP_RIGHT });
+      expect(gloPos.getPositions(hostElement)).toEqual({
+        right: 0,
+        top: 0,
+        width: 100,
+        height: 100,
+        position: 'fixed'
+      });
+    });
+  });
+  describe('should get correction position for', () => {
+    const targetElCoords = {
+      width: (window as any).innerWidth,
+      height: (window as any).innerHeight
+    };
+    console.log(targetElCoords);
+
+    const hostElCoords = {
+      width: 4, // actual 967
+      height: 10 // actual 18
+    };
+    getData().forEach(data => {
+      it(data.name, () => {
+        const gloPos = new GlobalPosition({
+          hostWidth: hostElCoords.width,
+          hostHeight: hostElCoords.height,
+          placement: data.placement,
+          offset: 2
+        });
+        const pos = (gloPos as any)[`calculate_${data.placement}`](targetElCoords, hostElCoords);
+        expect(pos).toEqual(data.expected);
+      });
+    });
+  });
+  describe('___TEST____', () => {
+    const targetElCoords = {
+      width: (window as any).innerWidth,
+      height: (window as any).innerHeight
+    };
+    console.log(targetElCoords);
+
+    const hostElCoords = {
+      width: 4, // actual 967
+      height: 10 // actual 18
+    };
+    getData().forEach(data => {
+      it(data.name, () => {
+        const gloPos = new GlobalPosition({
+          hostWidth: hostElCoords.width,
+          hostHeight: hostElCoords.height,
+          placement: data.placement,
+          offset: 2
+        });
+        const pos = (gloPos as any)._calc(data.placement, targetElCoords, hostElCoords);
+        expect(pos).toEqual(data.expected);
+      });
+    });
+  });
+  function getData() {
+    // offset is 2
+    ww = window.innerWidth;
+    wh = window.innerHeight;
+    const tests = [
+      {
+        name: 'bottom',
+        placement: InsidePlacement.BOTTOM,
+        method: `calculate_${InsidePlacement.BOTTOM}`,
+        expected: { left: (ww - 4) / 2, bottom: 2 }
+      },
+      {
+        name: 'top',
+        placement: InsidePlacement.TOP,
+        method: `calculate_${InsidePlacement.TOP}`,
+        expected: { left: (ww - 4) / 2, top: 2 }
+      },
+      {
+        name: 'left',
+        placement: InsidePlacement.LEFT,
+        method: `calculate_${InsidePlacement.LEFT}`,
+        expected: { left: 2, top: (wh - 10) / 2 }
+      },
+      {
+        name: 'right',
+        placement: InsidePlacement.RIGHT,
+        method: `calculate_${InsidePlacement.RIGHT}`,
+        expected: { right: 2, top: (wh - 10) / 2 }
+      },
+      {
+        name: 'center',
+        placement: InsidePlacement.CENTER,
+        method: `calculate_${InsidePlacement.CENTER}`,
+        expected: { left: (ww - 4) / 2, top: (wh - 10) / 2 }
+      },
+      {
+        name: 'top left',
+        placement: InsidePlacement.TOP_LEFT,
+        method: `calculate_${InsidePlacement.TOP_LEFT}`,
+        expected: { left: 2, top: 2 }
+      },
+      {
+        name: 'top right',
+        placement: InsidePlacement.TOP_RIGHT,
+        method: `calculate_${InsidePlacement.TOP_RIGHT}`,
+        expected: { right: 2, top: 2 }
+      },
+      {
+        name: 'bottom left',
+        placement: InsidePlacement.BOTTOM_LEFT,
+        method: `calculate_${InsidePlacement.BOTTOM_LEFT}`,
+        expected: { left: 2, bottom: 2 }
+      },
+      {
+        name: 'bottom right',
+        placement: InsidePlacement.BOTTOM_RIGHT,
+        method: `calculate_${InsidePlacement.BOTTOM_RIGHT}`,
+        expected: { right: 2, bottom: 2 }
+      }
+    ];
+
+    return tests;
+  }
+});
